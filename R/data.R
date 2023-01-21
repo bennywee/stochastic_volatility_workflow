beed8297f88ad2bad643612e770dbf8e8dfdffb7
occamsblazer3 <benw.cy93@gmail.com>
2023-01-21 22:17:18 +1100

    Generate data

--- R/data.R
+++ R/data.R
@@ -6,13 +6,54 @@ download_fin_data <- function(financial_series, start_date, end_date){
     getSymbols(series, env = data_env, src = "yahoo",
             from = as.Date(start_date), to = as.Date(end_date))
     data <- get("GSPC", envir = data_env)
-    return(data)
+    df <- as.data.frame(data)
+    df$Date  <- rownames(df)
+    return(df)
 }
 
-df <- download_fin_data(financial_series = "GSPC",
-                        start_date = "1960-01-04",
-                        end_date = "2023-01-01")
+fin_data_location <- function(financial_series, type, start_date, end_date){
+    path  <-  file.path("data", type, financial_series)
+    start  <-  format(strptime(as.character(start_date), "%Y-%m-%d"), "%Y%m%d")
+    end  <-  format(strptime(as.character(end_date), "%Y-%m-%d"), "%Y%m%d")
+    date_string  <-  paste(start, "_", end, ".csv", sep = "")
+    file_name <-  file.path(path, date_string)
+    return(file_name)
+}
 
-save_data <- function(name, start_date, end_date){
+save_fin_data <- function(dataframe, type, financial_series, start_date, end_date){
+    path  <-  file.path("data", type, financial_series)
+    
+    if (!dir.exists(path)) {
+        dir.create(path)
+    }
 
+    file_name <- fin_data_location(financial_series, type, start_date, end_date)
+
+    if (file.exists(file_name)) {
+        stop("Data file already exists")
+    } else {
+        write.csv(dataframe, file_name, row.names = FALSE)
+        print(paste("File successfully downloaded: ", file_name, sep = ""))
+    }
+}
+
+generate_dataset <- function(financial_series, data_type, start_date, end_date){
+
+    data <- download_fin_data(financial_series = financial_series,
+                        start_date = start_date,
+                        end_date = end_date)
+
+    save_fin_data(dataframe = data,
+                  financial_series = financial_series,
+                  type = data_type,
+                  start_date = start_date,
+                  end_date = end_date)
+
+}
+
+log_return <- function(data, column){
+    log_price = log(data[column])
+    lag_log_price = rbind(NA, head(log_price, -1))
+    log_returns <- log_price - lag_log_price
+    return(log_returns)
 }
---
+++ create_data.r
@@ -0,0 +1,27 @@
+# Load data functions
+source(file.path('R', 'data.R'))
+
+# (Download) and load data
+stock_index = "GSPC"
+start_date = "1960-01-04"
+end_date = "2023-01-01"
+raw_data_loc = fin_data_location(stock_index, "raw", start_date, end_date)
+preprocessed_data_loc = fin_data_location(stock_index, "preprocessed", start_date, end_date)
+
+if(file.exists(raw_data_loc)){
+    print("Data already downloaded.")
+} else {
+    generate_dataset(stock_index, "raw", start_date, end_date)
+}
+
+df <- read.csv(raw_data_loc)
+
+# Log transformations and save in the preprocessed dicretory
+df['log_price']  <- log(df["GSPC.Close"])
+df['log_return']  <- log_return(df, "GSPC.Close")
+
+if(file.exists(preprocessed_data_loc)){
+    print("Data already exists.")
+} else {
+    save_fin_data(df, "preprocessed", stock_index, start_date, end_date)
+}
