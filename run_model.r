80bda02a8a9da7a82aa8e05dfeb0e77fe4e5ce66
occamsblazer3 <benw.cy93@gmail.com>
2023-01-22 21:21:45 +1100

    Save model

--- run_model.r
+++ run_model.r
@@ -3,11 +3,16 @@ library(bayesplot)
 
 # Stan User guide SV model
 # Compile stan model
-file <- file.path("models", "sv_user_guide.stan")
+model_name  <- "sv_user_guide" 
+data_type  <-  "GSPC"
+file_name  <- "20100104_20230101"
+unique_identifier  <- "default_priors"
+
+file <- file.path("models", paste(model_name, ".stan", sep = ""))
 mod <- cmdstan_model(file)
 
 # Get data
-data <- read.csv("data/preprocessed/GSPC/20100104_20230101.csv")
+data <- read.csv(file.path("data", "preprocessed", data_type, paste(file_name, ".csv", sep = "")))
 log_returns  <-  data[complete.cases(data['log_return']), 'log_return']
 
 # Fit model
@@ -20,5 +25,7 @@ fit <- mod$sample(
   refresh = 500 # print update every 500 iters
 )
 
-fit$summary()
-mcmc_hist(fit$draws("mu"))
+# Save model
+fit_location  <- paste(model_name, "_", data_type, "_", file_name, "_", unique_identifier, sep = "")
+dir.create(file.path("output", fit_location), recursive = TRUE)
+fit$save_object(file = file.path("output", fit_location, "fit.RDS"))
