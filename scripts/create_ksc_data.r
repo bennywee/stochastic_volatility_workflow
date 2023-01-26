bbffb7a69f2e30b32380ea35bfd3ffbcaef75d58
occamsblazer3 <benw.cy93@gmail.com>
2023-01-26 22:49:17 +1100

    Added main script to create simulated data

---
+++ scripts/create_ksc_data.r
@@ -0,0 +1,36 @@
+# Load data functions
+source(file.path('R', 'data.R'))
+
+# Set parameters (mu is set within simulate_ksc function) and seed
+set.seed(323651)
+size <- 1000
+phi <-  0.97779
+sig <-  0.15850
+beta <- 0.64733
+
+# Set directory paths
+path <- file.path("data", "simulated", "ksc")
+
+data_name <- paste("phi_", as.character(phi), "_",
+                   "sig_", as.character(sig), "_",
+                   "beta_", as.character(beta),
+                   ".csv", sep ="")
+
+data_path <- file.path(path, data_name)
+
+# Simulate data
+df <- simulate_ksc(T=size,
+                   phi.true = phi,
+                   sig.true = sig,
+                   beta.true = beta)
+
+if (!dir.exists(path)) {
+        dir.create(path, recursive = TRUE)
+    }
+
+if (file.exists(data_path)) {
+        stop("Data file already exists")
+    } else {
+        write.csv(df, data_path, row.names = FALSE)
+        print(paste("File successfully downloaded: ", data_path, sep = ""))
+}
