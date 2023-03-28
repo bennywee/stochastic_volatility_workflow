f5083acd2d5e5d69e2f802d91e5c490fd3efb00c
Benjamin Wee <benw.cy93@gmail.com>
2023-03-28 15:27:48 +1100

    refactored using functions and reformatted

--- scripts/run_model.r
+++ scripts/run_model.r
@@ -1,91 +1,94 @@
 library(tidyverse)
 library(cmdstanr)
+source(here::here("R", "plots.R"))
+source(here::here("R", "model_eval.R"))
 
 set.seed(321321)
 
 ############################ Parameters to set ############################
 # Model description
-model_name  <- "sv_user_guide_reparameterised" 
-dependent_variable  <- "yobs"  #log squared returns
-unique_identifier  <- "default_priors_adapt_delta_0.9"
+model_name <- "sv_user_guide_reparameterised"
+dependent_variable <- "yobs" # log squared returns
+unique_identifier <- "default_priors_log_y_squared"
 
 # Data location
 data_loc <- "simulated"
-data_type  <-  "ksc"
-file_name  <- "phi_0.97779_sig_0.1585_beta_0.64733"
+data_type <- "ksc"
+file_name <- "phi_0.97779_sig_0.1585_beta_0.64733"
 ############################ Parameters to set ############################
 
-for (estimate in c(1,0)){
+for (prior in c(1, 0)) {
+  sample_prior <- prior # Prior predictive check (0) or estimate full model (1)
 
-sample_prior <- estimate # Prior predictive check (0) or estimate full model (1)
+  if (sample_prior == 1) {
+    prefix <- "Prior"
+  } else if (sample_prior == 0) {
+    prefix <- "Posterior"
+  } else {
+    stop("Estimation mode not set")
+  }
 
-if (sample_prior == 1){
-  prefix <- "Prior"
-} else if(sample_prior == 0) {
-  prefix <- "Posterior"
-} else {
-  stop("Estimation mode not set")
-}
+  # Stan User guide SV model
+  # Compile stan model
+  file <- here::here("models", paste(model_name, ".stan", sep = ""))
+  mod <- cmdstan_model(file)
 
-# Stan User guide SV model
-# Compile stan model
-file <- here::here("models", paste(model_name, ".stan", sep = ""))
-mod <- cmdstan_model(file)
+  # Get data
+  data <- read.csv(here::here("data", data_loc, data_type, paste(file_name, ".csv", sep = "")))
+  returns <- data[complete.cases(data[dependent_variable]), dependent_variable]
 
-# Get data
-data <- read.csv(here::here("data", data_loc, data_type, paste(file_name, ".csv", sep = "")))
-returns  <-  data[complete.cases(data[dependent_variable]), dependent_variable]
+  # Fit model
+  data_list <- list(T = length(returns), y = returns, sample_prior = sample_prior)
+  model_fit <- mod$sample(
+    data = data_list,
+    seed = 123,
+    chains = 4,
+    parallel_chains = 4,
+    refresh = 500,
+    adapt_delta = 0.9
+  )
 
-# Fit model
-data_list <- list(T = length(returns), y = returns, sample_prior = sample_prior)
-model_fit <- mod$sample(
-  data = data_list, 
-  seed = 123, 
-  chains = 4, 
-  parallel_chains = 4,
-  refresh = 500,
-  adapt_delta = 0.9
-)
+  # Save model
+  fit_location <- paste(model_name, "_", data_type, "_", file_name, "_", unique_identifier, sep = "") # "_", format(Sys.time(), "%Y%m%d%H%M%S"), sep = "")
+  path <- here::here("output", fit_location)
+  if (!dir.exists(path)) {
+    dir.create(path, recursive = TRUE)
+  }
 
-# Save model
-fit_location  <- paste(model_name, "_", data_type, "_", file_name, "_", unique_identifier, sep = "")# "_", format(Sys.time(), "%Y%m%d%H%M%S"), sep = "")
-path <- here::here("output", fit_location)
-if (!dir.exists(path)) {
-        dir.create(path, recursive = TRUE)
-    }
+  model_fit$save_object(file = here::here("output", fit_location, paste(tolower(prefix), "fit.RDS", sep = "")))
+  model_fit$save_output_files(dir = path, basename = tolower(prefix))
+  model_fit$save_data_file(dir = path)
 
-model_fit$save_object(file = here::here("output", fit_location, paste(tolower(prefix),"fit.RDS", sep ="")))
-model_fit$save_output_files(dir = path, basename = tolower(prefix))
-model_fit$save_data_file(dir = path)
+  # Evaluation
+  # Predictive check
+  y_rep_df_sample <- mcmc_output_df(model_obj = model_fit, variable = "y_rep", sample_n = 10)
+  log_y_squared_avg <- average_over_draws(model_obj = model_fit, variable = "log_y_squared")
+  true_returns_df <- returns_df(returns)
+  true_log_sq_returns <- log_squared_returns(returns)
 
-# Evaluation
-# Prepare generated data for predictive plots
-y_rep_df <- model_fit$draws(variable = "y_rep", format = "matrix") %>% 
-  t() %>% 
-  as.data.frame() %>% 
-  mutate(time = 1:nrow(.)) %>% 
-  pivot_longer(!time, names_to = "mcmc_draw", values_to = "y_t")
+  plot_predictive_check(
+    dataframe = y_rep_df_sample,
+    x_axis = time,
+    y_axis = y_t,
+    groups = mcmc_draw,
+    prior_post = prefix,
+    save = TRUE,
+    path = fit_location,
+    true_data = true_returns_df
+  )
 
-# Predictive check
-y_rep_df_sample  <- y_rep_df  %>% 
-  filter(mcmc_draw %in% sample(1:4000, 10))
+plot_log_y_sqd_hist(mcmc_data = log_y_squared_avg,
+                    true_data = true_log_sq_returns, 
+                    mcmc_x_axis = average, 
+                    true_x_axis = log_y_squared, 
+                    prior_post = prefix, 
+                    save = FALSE,
+                    path = fit_location) 
 
-true_data <- returns %>% 
-  as.data.frame() %>% 
-  mutate(time = 1:1000)
-
-colnames(true_data)  <- c("y_t", "time")
-
-ggplot(y_rep_df_sample, aes(x = time, y = y_t)) +
-    geom_line(aes(group = mcmc_draw), alpha = 0.3, colour = "blue") +
-    geom_line(data = true_data, aes(x=time, y = y_t)) +
-    labs(title = paste(prefix, " Predictive Checks"),
-         subtitle = "10 blue MCMC draws, true value in black") +
-    theme_minimal()
-
-ggsave(here::here("output", fit_location, paste(tolower(prefix),"_predictive_check.png", sep ="")), 
-       bg="white",
-       width = 8,
-       height = 5,
-       units = "in")
-}
+plot_log_y_sqd_kde(mcmc_data = log_y_squared_avg,
+                    true_data = true_log_sq_returns, 
+                    mcmc_x_axis = average, 
+                    true_x_axis = log_y_squared, 
+                    prior_post = prefix, 
+                    save = TRUE,
+                    path = fit_location) 
