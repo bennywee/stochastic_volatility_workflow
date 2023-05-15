98da6cbfb7f58b9631b36fb09a8c6b884f9951d4
Benjamin Wee (Monash University) <benjamiw@monarch-login1.erc.monash.edu>
2023-05-15 16:19:12 +1000

    change name

--- job.sh
+++ job.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=test_sim_bw
+#SBATCH --job-name=sv_adapt_delta_sim
 #SBATCH --time=01:00:00
-#SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=4096
 #SBATCH --cpus-per-task=4
 #SBATCH --array=1-100
