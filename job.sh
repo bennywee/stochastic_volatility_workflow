9b1a695d40b23fbf7c82f366f297329577c75dcd
Benjamin Wee (Monash University) <benjamiw@monarch-login1.erc.monash.edu>
2023-06-04 12:17:38 +1000

    increased cpu memory

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=sv_adapt_delta_sim
+#SBATCH --job-name=adapt_delta_0.9_0.99_0.005_sigsqd_prior_dataset_100
 #SBATCH --time=03:00:00
-#SBATCH --mem-per-cpu=4096
+#SBATCH --mem-per-cpu=8096
 #SBATCH --cpus-per-task=4
 #SBATCH --array=1-100
 
