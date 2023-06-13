b37d74a384d87580beacffd09313eb3bae6bcc40
Benjamin Wee <benw.cy93@gmail.com>
2023-06-13 19:10:11 +1000

    comments

--- analysis/sbc/sbc.r
+++ analysis/sbc/sbc.r
@@ -13,3 +13,14 @@ sv_fit <- sv_mod$sample(
 )
 
 sv_fit$draws(variables = c('sim_ranks'), format = 'df')
+
+# Getting lots of Chain 1 Exception: normal_lpdf: Scale parameter[1] is nan, but must be positive
+# errors. Doesn't happen when posterior sampling in previous models.
+# But it happened with prior sampling in the past.
+# Maybe need to be more specific about prior choice. Particularly with the
+# Reparameterised model.
+# Probably should check when this error occurred previously.
+# Maybe posterior sampling pushed the warmup into regions where it doesn't sample
+# a value which gives -inf (exp(o)).
+# And sampling from prior puts too much weight on values that shouldn't exist.
+# Look at how reparameterised model is derived mathematically. see if it makes sense
