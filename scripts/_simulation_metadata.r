5d6360fc97e7fd72f567211b1d96ce3e6e683150
Benjamin Wee <benw.cy93@gmail.com>
2023-06-14 20:01:57 +1000

    commandArgs

--- scripts/_simulation_metadata.r
+++ scripts/_simulation_metadata.r
@@ -1,14 +1,15 @@
-source(here::here("configs", "adapt_delta_sim.r"))
+args = commandArgs(trailingOnly = TRUE)
+source(here::here("configs", args[1]))
 
 path <- here::here("simulation_output", simulation_name, "output")
 
 if (!dir.exists(path)) {
-    dir.create(path, recursive = TRUE)
-  }
+  dir.create(path, recursive = TRUE)
+}
 
 # Use writeLines to pretty print this text
 stan_code <- readLines(here::here("models", paste(model_name, ".stan", sep = "")))
-config_file <- readLines(here::here("configs", "adapt_delta_sim.r"))
+config_file <- readLines(here::here("configs", args[1]))
 simulation_description <- simulation_description
 metadata <- list(stan = stan_code, config = config_file, description = simulation_description)
 saveRDS(metadata, file = here::here(here::here("simulation_output", simulation_name, "metadata.RDS")))
