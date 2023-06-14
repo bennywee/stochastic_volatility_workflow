7cc0969cbeef34ec58b56f3eaa497189267b654b
Benjamin Wee <benw.cy93@gmail.com>
2023-06-14 19:20:30 +1000

    sum ranks

--- analysis/sbc/sbc.r
+++ analysis/sbc/sbc.r
@@ -1,18 +1,32 @@
 library(cmdstanr)
+library(dplyr)
 
 sv_mod <- cmdstan_model(here::here("models", "sv_ncp_ksc_svb.stan"),
     dir = here::here("models", "executables"))
 
 sv_fit <- sv_mod$sample(
     seed = 123,
-    chains = 4,
-    parallel_chains = 4,
+    chains = 1,
+    parallel_chains = 1,
     refresh = 500,
     adapt_delta = 0.95,
-    save_warmup = FALSE
+    save_warmup = FALSE,
+    num_samples = 999
 )
 
-sv_fit$draws(variables = c('sim_ranks'), format = 'df')
+ranks <- sv_fit$draws(variables = c('sim_ranks'), format = 'df') %>% select(-c(.chain, .iteration, .draw))
+params <- c("mu",
+    "phi",
+    "sigma",
+    "h_1",
+    "h_100",
+    "h_400",
+    "h_500",
+    "h_600",
+    "h_1000"
+)
+names(ranks) <- params
+agg_ranks <- apply(ranks, 2, FUN = sum)
 
 # Getting lots of Chain 1 Exception: normal_lpdf: Scale parameter[1] is nan, but must be positive
 # errors. Doesn't happen when posterior sampling in previous models.
