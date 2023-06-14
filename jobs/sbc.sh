b5398276779c81831eb993093acd0cccad1882ba
Benjamin Wee <benw.cy93@gmail.com>
2023-06-14 19:45:56 +1000

    job script

---
+++ jobs/sbc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --job-name=sbc_ncp_ksc_priors_0.99_adapt_delta
+#SBATCH --time=01:00:00
+#SBATCH --mem-per-cpu=4096
+#SBATCH --cpus-per-task=1
+#SBATCH --array=1-999
+
+R --vanilla < scripts/sim_sbc.r
