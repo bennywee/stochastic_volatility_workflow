b06d07c431b16029da5757a5f78e09e938c44ad6
Benjamin Wee <benw.cy93@gmail.com>
2023-06-14 19:45:44 +1000

    sbc config

---
+++ configs/sbc_sim.r
@@ -0,0 +1,27 @@
+simulation_name <- "sbc_ncp_ksc_priors_0.99_adapt_delta"
+simulation_description = "
+SBC with non centereed model and ksc priors
+"
+
+# Model metadata
+model_name <- "sv_user_guide_reparameterised_ksc_priors" # Name of the .stan file in the models directory
+
+## Stan parameters
+chains <- 1
+parallel_chains <- 1
+refresh <- 500
+adapt_delta <- 0.99
+save_warmup <- FALSE
+iter_sampling <- 999
+
+## Model parameters to track
+params <- c("mu",
+    "phi",
+    "sigma",
+    "h_1",
+    "h_100",
+    "h_400",
+    "h_500",
+    "h_600",
+    "h_1000"
+)
