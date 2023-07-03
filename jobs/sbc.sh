f5d6de29f2cc79a5f2e3bed4848dd4cec8b114e1
Benjamin Wee <benw.cy93@gmail.com>
2023-07-03 14:43:39 +1000

    increased cpu memory

--- jobs/sbc.sh
+++ jobs/sbc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=sbc_ncp_ksc_priors_0.99_adapt_delta
 #SBATCH --time=01:00:00
-#SBATCH --mem-per-cpu=4096
+#SBATCH --mem-per-cpu=8096
 #SBATCH --cpus-per-task=1
 #SBATCH --array=1-999
 
