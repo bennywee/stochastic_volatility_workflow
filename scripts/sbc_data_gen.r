0afb68e5bf1429a28564eb660336720766dcecd3
Benjamin Wee (Monash University) <benjamiw@monarch-login2.erc.monash.edu>
2023-07-15 11:43:45 +1000

    save config

--- scripts/sbc_data_gen.r
+++ scripts/sbc_data_gen.r
@@ -3,12 +3,12 @@ library(cmdstanr)
 T <- 1000
 n_iterations <- 999
 model_name <- "sbc_data_gen_sv_ncp_ksc_priors"
-data_path <- here::here("data", "simulated", "sbc")
+data_path <- here::here("data", "simulated", "sbc", model_name)
 seed <- 543
 
 executables_path <- here::here("models/executables")
 if (!dir.exists(executables_path)) dir.create(executables_path)
-if (!dir.exists(data_path)) dir.create(data_path)
+if (!dir.exists(data_path)) dir.create(data_path, recursive = TRUE)
 
 file <- here::here("models", paste(model_name, ".stan", sep = ""))
 mod <- cmdstan_model(file, include_paths = here::here("models", "functions"), dir = executables_path)
@@ -34,3 +34,8 @@ for (i in 1:n_iterations) {
     names(data) <- c("y_sim")
     write.csv(data, file = paste(data_path, paste(i, "csv", sep = "."), sep = "/"))
 }
+
+stan_code <- file
+config_file <- readLines(here::here("scripts", "sbc_data_gen.r"))
+metadata <- list(stan = stan_code, config = config_file)
+saveRDS(metadata, file = here::here(here::here(data_path, "metadata.RDS")))
