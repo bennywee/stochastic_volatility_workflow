cc8c89ed764bb8400ddaeedf724bd5d3599a5f0f
Benjamin Wee <benw.cy93@gmail.com>
2023-09-28 20:48:20 +1000

    Extra histograms and ess plots

---
+++ manuscript/results/ess.r
@@ -0,0 +1,69 @@
+library(tidyverse)
+source("R/sbc.r")
+static_parameters <- c("~mu", "~sigma^2", "~phi")
+
+# HMC results
+# 5000 iterations CP
+path <- "simulation_output/stan/cp/sbc_cp_ksc_priors_0.999_adapt_delta_premade_datasets_r1_5000_iterations"
+rds_list <- list.files(path = paste(path, "output", sep = "/"), pattern = "*.RDS")
+rds_path <- paste("output", rds_list, sep = "/")
+
+hmc_cp_ess_basic_5k <- future_lapply(rds_path,
+                           get_ess, 
+                           model_path = path, 
+                           ess_type = "ess_basic", 
+                           chains = "one_chain", 
+                           future.seed = NULL)
+
+hmc_cp_ess_basic_5k_df <- as.data.frame(do.call("rbind", hmc_cp_ess_basic_5k))
+hmc_cp_ess_basic_5k_df <- latex_variables(hmc_cp_ess_basic_5k_df)
+hmc_cp_ess_basic_5k_df$Type <- "Centered"
+
+
+# 5000 iterations NCP
+path <- "simulation_output/stan/ncp/sbc_ncp_ksc_priors_0.999_adapt_delta_premade_datasets_r9_5000_iterations"
+rds_list <- list.files(path = paste(path, "output", sep = "/"), pattern = "*.RDS")
+rds_path <- paste("output", rds_list, sep = "/")
+
+hmc_ncp_ess_basic_5k <- future_lapply(rds_path,
+                           get_ess, 
+                           model_path = path, 
+                           ess_type = "ess_basic", 
+                           chains = "one_chain", 
+                           future.seed = NULL)
+
+hmc_ncp_ess_basic_5k_df <- as.data.frame(do.call("rbind", hmc_ncp_ess_basic_5k))
+hmc_ncp_ess_basic_5k_df <- latex_variables(hmc_ncp_ess_basic_5k_df)
+hmc_ncp_ess_basic_5k_df$Type <- "Non Centered"
+
+hmc_ess_df <- rbind(hmc_cp_ess_basic_5k_df, hmc_ncp_ess_basic_5k_df) %>% 
+    filter(parameters %in% static_parameters)
+
+hmc_ess_df <- latex_variables(hmc_ess_df)
+
+plot <- ggplot(hmc_ess_df, aes(x = parameters, y = ess_basic, fill = Type)) +
+    geom_boxplot() +
+    theme_minimal(base_size = 22) +
+        labs(x = "Parameters",
+             y = "Count")
+
+x_labs = ggplot_build(plot)$layout$panel_params[[1]]$x$get_labels()
+plot <- plot + scale_x_discrete(labels = parse(text = x_labs))
+ggsave("manuscript/results/hmc_ess.png", bg = "white", width = 14, height = 9.42)
+
+
+
+
+
+# hmc_1k_basic <- hmc_cp_ess_basic_df %>%
+#         filter(parameters %in% static_parameters) %>% 
+#         ggplot(.) +
+#         geom_boxplot(aes(x = parameters, y = ess_basic)) +
+#         theme_minimal(base_size = 22) +
+#         labs(x = "Parameters",
+#              y = "Count")
+
+# x_labs = ggplot_build(hmc_1k_basic)$layout$panel_params[[1]]$x$get_labels()
+# hmc_1k_basic <- hmc_1k_basic + scale_x_discrete(labels = parse(text = x_labs))
+
+#     return(plot)
--- manuscript/results/rank_histograms.r
+++ manuscript/results/rank_histograms.r
@@ -1,6 +1,7 @@
 library(tidyverse)
 source("R/sbc.r")
 
+n_iterations <- 1000
 posterior_samples <- 999 # Posterior samples. m+1 possible ranks. So want (m+1)/J = expected samples for uniform dist.
 n_bins <- 20 # Bins
 expected_count <- n_iterations / n_bins # Chi sqd stat
@@ -49,7 +50,7 @@ static_rank_bins <- function(data, variables, type){
     return(result)
 }
 
-
+# HMC 1k
 hmc_cp_onek_bins <- rank_bins_f(path = "simulation_output/stan/cp/sbc_cp_ksc_priors_0.999_adapt_delta_premade_datasets_r1_1000_iterations", 
                                 rank_type = "agg_ranks", 
                                 posterior_samples = 999, 
@@ -76,13 +77,129 @@ ggplot(hmc_1k) +
         theme_minimal(base_size = 20) +
         theme(strip.text.x = element_text(size = 22)) +
         geom_hline(yintercept = expected_count, linewidth = 0.5, alpha = 0.3) +
-        labs(title = "Distribution of Rank Statistics",
-                y = "Count",
-                x = "Ranks")
+        labs(y = "Count",
+             x = "Ranks")
+
+ggsave("manuscript/results/hmc_1k.png", bg = "white", width = 14, height = 9.42)
+
+# HMC 5k
+n_iterations <- 5000
+posterior_samples <- 999 # Posterior samples. m+1 possible ranks. So want (m+1)/J = expected samples for uniform dist.
+n_bins <- 20 # Bins
+expected_count <- n_iterations / n_bins # Chi sqd stat
+
+hmc_cp_fivek_bins <- rank_bins_f(path = "simulation_output/stan/cp/sbc_cp_ksc_priors_0.999_adapt_delta_premade_datasets_r1_5000_iterations", 
+                                rank_type = "agg_ranks", 
+                                posterior_samples = 999, 
+                                n_bins= 20)
+
+hmc_ncp_fivek_bins <- rank_bins_f(path = "simulation_output/stan/ncp/sbc_ncp_ksc_priors_0.999_adapt_delta_premade_datasets_r9_5000_iterations", 
+                                rank_type = "agg_ranks", 
+                                posterior_samples = 999, 
+                                n_bins= 20)
+
+hmc_cp_fivek_bin_long <- static_rank_bins(data = hmc_cp_fivek_bins, 
+                                         variables = c("~mu", "~sigma^2", "~phi"), 
+                                         type = "Centered")
+hmc_ncp_fivek_bin_long <- static_rank_bins(data = hmc_ncp_fivek_bins, 
+                                          variables = c("~mu", "~sigma^2", "~phi"), 
+                                          type = "NonCentered")
+
+hmc_5k = rbind(hmc_cp_fivek_bin_long, hmc_ncp_fivek_bin_long)
+
+ggplot(hmc_5k) +
+        geom_histogram(aes(rank), bins = 20, fill = "light blue", alpha = 0.7) +
+        scale_x_continuous(labels = function(x) x * (posterior_samples+1)/n_bins) +
+        facet_grid(type~variable, labeller = label_parsed) +
+        theme_minimal(base_size = 20) +
+        theme(strip.text.x = element_text(size = 22)) +
+        geom_hline(yintercept = expected_count, linewidth = 0.5, alpha = 0.3) +
+        labs(x = "Ranks",
+             y = "Count")
+
+ggsave("manuscript/results/hmc_5k.png", bg = "white", width = 14, height = 9.42)
+
+# KSC 1k
+n_iterations <- 1000
+posterior_samples <- 9999 # Posterior samples. m+1 possible ranks. So want (m+1)/J = expected samples for uniform dist.
+n_bins <- 20 # Bins
+expected_count <- n_iterations / n_bins # Chi sqd stat
+
+ksc_cp_onek_bins <- rank_bins_f(path = "simulation_output/ksc/cp/sbc_cp_ksc_model_cp_dgf_10kmcmc_1000iter_r2", 
+                                    rank_type = "agg_ranks", 
+                                    posterior_samples = posterior_samples, 
+                                    n_bins= n_bins)
+
+ksc_cp_onek_bins_long <- static_rank_bins(data = ksc_cp_onek_bins, 
+                                         variables = c("~mu", "~sigma^2", "~phi"), 
+                                         type = "Centered")
 
 
+ggplot(ksc_cp_onek_bins_long) +
+        geom_histogram(aes(rank), bins = 20, fill = "light blue", alpha = 0.7) +
+        scale_x_continuous(labels = function(x) x * (posterior_samples+1)/n_bins) +
+        facet_grid(~variable, labeller = label_parsed) +
+        theme_minimal(base_size = 20) +
+        theme(strip.text.x = element_text(size = 22)) +
+        geom_hline(yintercept = expected_count, linewidth = 0.5, alpha = 0.3) +
+        labs(x = "Ranks", 
+             y = "Count")
 
+ggsave("manuscript/results/ksc_1k.png", bg = "white", width = 14, height = 5.42)
 
+# SIR 1k
+n_iterations <- 1000
+posterior_samples <- 9999 # Posterior samples. m+1 possible ranks. So want (m+1)/J = expected samples for uniform dist.
+n_bins <- 20 # Bins
+expected_count <- n_iterations / n_bins # Chi sqd stat
+
+sir_ksc_cp_onek_bins <- rank_bins_f(path = "simulation_output/ksc/cp/sir_sbc_cp_ksc_model_cp_dgf_10kmcmc_1000iter_r3", 
+                                    rank_type = "agg_ranks", 
+                                    posterior_samples = posterior_samples, 
+                                    n_bins= n_bins)
+
+sir_ksc_cp_onek_bins_long <- static_rank_bins(data = sir_ksc_cp_onek_bins, 
+                                         variables = c("~mu", "~sigma^2", "~phi"), 
+                                         type = "Centered")
+
+ggplot(sir_ksc_cp_onek_bins_long) +
+        geom_histogram(aes(rank), bins = 20, fill = "light blue", alpha = 0.7) +
+        scale_x_continuous(labels = function(x) x * (posterior_samples+1)/n_bins) +
+        facet_grid(~variable, labeller = label_parsed) +
+        theme_minimal(base_size = 20) +
+        theme(strip.text.x = element_text(size = 22)) +
+        geom_hline(yintercept = expected_count, linewidth = 0.5, alpha = 0.3) +
+        labs(x = "Ranks",
+             y = "Count")
+
+ggsave("manuscript/results/sir_1k.png", bg = "white", width = 14, height = 5.42)
+
+# SIR 5k
+n_iterations <- 5000
+posterior_samples <- 9999 # Posterior samples. m+1 possible ranks. So want (m+1)/J = expected samples for uniform dist.
+n_bins <- 20 # Bins
+expected_count <- n_iterations / n_bins # Chi sqd stat
+
+sir_ksc_cp_fivek_bins <- rank_bins_f(path = "simulation_output/ksc/cp/sir_sbc_cp_ksc_model_cp_dgf_10kmcmc_5000iter_r1", 
+                                    rank_type = "agg_ranks", 
+                                    posterior_samples = posterior_samples, 
+                                    n_bins= n_bins)
+
+sir_ksc_cp_fivek_bins_long <- static_rank_bins(data = sir_ksc_cp_fivek_bins, 
+                                         variables = c("~mu", "~sigma^2", "~phi"), 
+                                         type = "Centered")
+
+ggplot(sir_ksc_cp_fivek_bins_long) +
+        geom_histogram(aes(rank), bins = 20, fill = "light blue", alpha = 0.7) +
+        scale_x_continuous(labels = function(x) x * (posterior_samples+1)/n_bins) +
+        facet_grid(~variable, labeller = label_parsed) +
+        theme_minimal(base_size = 20) +
+        theme(strip.text.x = element_text(size = 22)) +
+        geom_hline(yintercept = expected_count, linewidth = 0.5, alpha = 0.3) +
+        labs(x = "Ranks",
+             y = "Count")
+
+ggsave("manuscript/results/sir_5k.png", bg = "white", width = 14, height = 5.42)
 
 
 
